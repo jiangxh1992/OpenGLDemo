345c8f37f7c09c403588b8c4bf532f729fc7d781
WINDOWS10 <919575700@qq.com>
2016-09-24 21:45:57 +0800

    OpenGLDemo基础工程，tutorial10；

---
+++ OpenGLDemo/stdafx.h
@@ -0,0 +1,29 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+// TODO:  在此处引用程序需要的其他头文件
+#include <GL/glew.h>
+#pragma comment(lib, "glew32d.lib")
+
+#include <freeglut.h>
+
+#include "ogldev_math_3d.h"
+
+#include "ogldev_util.h"
+
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+
+//#include <sys/time.h>
